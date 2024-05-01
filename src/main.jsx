93ba18cee782f469081d1ec0ea00bac9c65da5ee
PhongDiep <phongdiep0712@gmail.com>
2024-04-30 17:22:41 -0700

    Changes some minor stuffs

--- src/main.jsx
+++ src/main.jsx
@@ -7,21 +7,6 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import { extendTheme } from '@chakra-ui/react'
-import {mode} from '@chakra-ui/theme-tools'
-// const config = {
-//   initialColorMode: 'dark',
-//   useSystemColorMode: false,
-// }
-// const styles = {
-//   global: (props) => ({
-//     body: {
-//       bg:mode("gray.100", "#000")(props),
-//       color: mode("gray.800", "whiteAlpha.900")(props)
-//     }
-//   })
-// }
-// const theme = extendTheme({ config, styles })
 const router = createBrowserRouter([
   {
     path: '/',
